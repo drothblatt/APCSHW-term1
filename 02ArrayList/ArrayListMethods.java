71c769864eafe4dc3ccaaefcb10b853386b071ae
David Rothblatt <daroth13@gmail.com>
2014-11-13 18:43:36 -0500

    randomize(L) is fully functional; main method had been adjusted as appropriate

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class ArrayListMethods{
     
-    // Modify the list so that consecutive duplicates are removed
     public static void collapseDuplicates(ArrayList<Integer> L){ // went with static. 
 	for (int i = 0; i < L.size()-1; i++){
 	    if (L.get(i).equals(L.get(i+1))){
@@ -27,18 +26,27 @@ public class ArrayListMethods{
 	ArrayList<Integer> L = new ArrayList<Integer>();
 	
 	// Testing collapseDuplicates(L)...
-	for (int i = 0; i < 15; i += r.nextInt(5) ){
+
+	for (int i = r.nextInt(5)+1; i < 32; i += r.nextInt(8) ){
 	    int reps = r.nextInt(4)+1;
 	    while (reps > 0){
 		L.add(new Integer(i));
 		reps --;
 	    }
 	}
+	
+	System.out.println("Testing collapseDuplicates(L)");
 	System.out.println(L); //toString() not needed. cool. 
 	collapseDuplicates(L);
        	System.out.println(L);
 
-	//
+	// Testing randomize(L)...
+
+	System.out.println("\nTesting randomize(L)");
+       	System.out.println(L);
+	randomize(L); // no issues with using the list already made. 
+	System.out.println(L);
+      
     }
 }
     
