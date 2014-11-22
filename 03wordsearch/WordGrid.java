118a771539151b434e6b3e15fe2cd5a5d001fc34
David Rothblatt <daroth13@gmail.com>
2014-11-21 23:57:31 -0500

    created reverse fxn. tested it.

--- 03wordsearch/WordGrid.java
+++ 03wordsearch/WordGrid.java
@@ -137,9 +137,46 @@ public class WordGrid{
 	return result;
     }
 
+    public String reverse(String word){
+	String reverse = "";
+	for (int i = word.length()-1; i >= 0; i--){
+	    reverse += word.charAt(i);
+	}
+	return reverse;
+    }
+	    
+	    
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from left to right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+
+    //public boolean addWordHorizontalRev(String word, int row, int col){
+	
+    // }
+
+
+
+
+
+
+
+
+
+
     // Time to check!
     public static void main(String[] args){
 	WordGrid firstTry = new WordGrid(5,5);
+
+	System.out.println("Expecting Watermelon: " + firstTry.reverse("Watermelon"));
+
 	System.out.println(firstTry.toString());
 	firstTry.clear();
 	System.out.println("new box..." + firstTry.toString() + "...end of new box \n");
